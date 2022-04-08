7e5ca9f355abad965ac9a09053bc3280c75d4cee
efremjon <efremyohanis@gmail.com>
2022-04-08 15:32:37 -0700

    efrem make upadet

--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,11 @@
+import re
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+def login_page(request):
+    return render(request,'Account/pages-login.html',{})
+
+def register_view(request):
+    if request.method == 'POST':
+        firstname=request.POST['first_name']
+    return render(request,'Account/pages-register.html',{})
