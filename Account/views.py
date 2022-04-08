d9aa217f21aee1705f54f278933dab1cc82b06f1
efremjon <efremyohanis@gmail.com>
2022-04-08 15:39:08 -0700

    efrem make another change

--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,4 @@
+import email
 import re
 from django.shortcuts import render
 from django.contrib.auth.models import User
@@ -8,4 +9,9 @@ def login_page(request):
 def register_view(request):
     if request.method == 'POST':
         firstname=request.POST['first_name']
+        lastname=request.POST['last_name']
+        username=request.POST['username']
+        email=request.POST['email']
+        passord1=request.POST['password1']
+        passord2=request.POST['password2']
     return render(request,'Account/pages-register.html',{})
